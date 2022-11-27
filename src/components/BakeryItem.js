431f691abc648d5eeef29410f4e191148226622a
ohrtann19 <anna_ohrt@brown.edu>
2022-11-26 17:47:31 -0800

    finished studio

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,14 @@
 // TODO: create a component that displays a single bakery item
+
+export default function BakeryItem({item, onClick}) {
+    return <div className="BakeryItem">
+        <h2>{item.name}</h2>
+        <img 
+        src={item.image} 
+        alt={item.description}
+      />
+      <p>{item.description}</p>
+      <h3>{item.price}</h3>
+      <button onClick={() => onClick(item)}>Add to Cart</button>
+    </div>
+}
